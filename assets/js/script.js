b46e3e57f7e28497dd7f7d634efd0f98b0c89bc1
Zack Jones <zajonesck83@gmail.com>
2024-02-27 20:36:54 -0600

    first big push

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,8 @@
-'use strict';
-
-
+"use strict";
 
 /**
  * navbar toggle
  */
-
 const header = document.querySelector("[data-header]");
 const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
 
@@ -17,7 +14,6 @@ navToggleBtn.addEventListener("click", function () {
 /**
  * toggle the navbar when click any navbar link
  */
-
 const navbarLinks = document.querySelectorAll("[data-nav-link]");
 
 for (let i = 0; i < navbarLinks.length; i++) {
@@ -27,14 +23,9 @@ for (let i = 0; i < navbarLinks.length; i++) {
   });
 }
 
-
-
-
-
 /**
  * back to top & header
  */
-
 const backTopBtn = document.querySelector("[data-back-to-top]");
 
 window.addEventListener("scroll", function () {
@@ -45,4 +36,41 @@ window.addEventListener("scroll", function () {
     header.classList.remove("active");
     backTopBtn.classList.remove("active");
   }
+});
+
+// Countdown Timer Script
+// Set the date we're counting down to
+var countDownDate = new Date("2024-12-25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="timer"
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "Election Day!";
+  }
+}, 1000);
+
+// Dynamically update the year in copy right
+document.addEventListener("DOMContentLoaded", function () {
+  var year = new Date().getFullYear(); // Gets the current year
+  document.getElementById("year").textContent = year; // Sets the year in the HTML
 });
