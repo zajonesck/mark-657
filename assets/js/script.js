d65a6237b20d05af8a74de33f08efb98f3af0ef1
Zack Jones <zajonesck83@gmail.com>
2024-04-11 15:39:48 -0500

    updated email contact form

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,38 +39,30 @@ window.addEventListener("scroll", function () {
 });
 
 // Countdown Timer Script
-// Set the date we're counting down to
 var countDownDate = new Date("2024-12-25").getTime();
 
-// Update the count down every 1 second
 var x = setInterval(function () {
-  // Get today's date and time
   var now = new Date().getTime();
 
-  // Find the distance between now and the count down date
   var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Output the result in an element with id="timer"
   document.getElementById("days").innerHTML = days;
   document.getElementById("hours").innerHTML = hours;
   document.getElementById("minutes").innerHTML = minutes;
   document.getElementById("seconds").innerHTML = seconds;
 
-  // If the count down is over, write some text
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("timer").innerHTML = "Election Day!";
   }
 }, 1000);
 
-// Dynamically update the year in copy right
 document.addEventListener("DOMContentLoaded", function () {
-  var year = new Date().getFullYear(); // Gets the current year
-  document.getElementById("year").textContent = year; // Sets the year in the HTML
+  var year = new Date().getFullYear();
+  document.getElementById("year").textContent = year;
 });
